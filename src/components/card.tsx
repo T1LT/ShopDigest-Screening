084a97474b91ecf4eaa93e2aa4ec7d47ff3ca773
Nishant Racherla <nishant.racherla@gmail.com>
2024-08-28 20:58:24 +0530

    style: fix card text overflow

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -17,9 +17,9 @@ type Collection = {
 export default function Card({ collection }: { collection: Collection }) {
   return (
     <div className="h-[250px] p-4 flex flex-col justify-between border rounded-lg">
-      <div className="flex flex-col gap-2">
-        <h1 className="font-bold">{collection.title}</h1>
-        <p className="text-sm text-neutral-700">{collection.description}</p>
+      <div className="flex flex-col gap-2 mb-4">
+        <h1 className="font-bold overflow-hidden text-ellipsis line-clamp-2">{collection.title}</h1>
+        <p className="text-sm text-neutral-700 overflow-hidden text-ellipsis line-clamp-3">{collection.description}</p>
       </div>
       <div className="divide-y">
         <div className="flex items-center gap-2 pb-4">
@@ -27,7 +27,7 @@ export default function Card({ collection }: { collection: Collection }) {
           <Image src="/logos/snapchat.png" alt="snapchat logo" height={32} width={32} className="rounded-lg" />
           <Image src="/logos/twitter.png" alt="twitter logo" height={32} width={32} className="rounded-lg" />
         </div>
-        <div className="flex items-center justify-between pt-2">
+        <div className="flex items-center justify-between pt-3">
           <span className="text-sm text-purple-600 font-medium">{collection.rating.count} Apps</span>
           <button className="flex items-center gap-2 px-2.5 py-1.5 text-xs bg-black text-white rounded-full hover:bg-neutral-700 transition">
             Explore more
