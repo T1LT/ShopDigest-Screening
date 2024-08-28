8c96b91ec6bc9a65c0d3ea1b2bb4f49590fda4da
Nishant Racherla <nishant.racherla@gmail.com>
2024-08-28 20:04:05 +0530

    feat: switch to fakeapi

--- src/app/CardsGrid.tsx
+++ src/app/CardsGrid.tsx
@@ -1,25 +1,27 @@
-import { promises as fs } from "fs";
 import Card from "@/components/card";
 
 type Collection = {
+  id: number;
   title: string;
   description: string;
-  numApps: number;
+  price: number;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  }
 };
 
 export default async function CardsGrid() {
-  // reading data from local json file
-  // in the actual code, we use server actions or client component + useEffect to fetch collections
-  const file = await fs.readFile(process.cwd() + "/public/collections.json", "utf8");
-  const collections = JSON.parse(file);
+  const res = await fetch("https://fakestoreapi.com/products/");
+  const collections = await res.json();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
       {collections ? collections.map((collection: Collection, idx: number) => (
         <Card
-          title={collection.title}
-          description={collection.description}
-          numApps={collection.numApps}
+          collection={collection}
           key={idx}
         />
       )) : <></>}
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 
-export default function Card({ title, description, numApps }: { title: string, description: string, numApps: number }) {
+type Collection = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  }
+};
+
+export default function Card({ collection }: { collection: Collection }) {
   return (
     <div className="h-[250px] p-4 flex flex-col justify-between border rounded-lg">
       <div className="flex flex-col gap-2">
-        <h1 className="font-bold">{title}</h1>
-        <p className="text-sm text-neutral-700">{description}</p>
+        <h1 className="font-bold">{collection.title}</h1>
+        <p className="text-sm text-neutral-700">{collection.description}</p>
       </div>
       <div className="divide-y">
         <div className="flex items-center gap-2 pb-4">
@@ -15,7 +28,7 @@ export default function Card({ title, description, numApps }: { title: string, d
           <Image src="/logos/twitter.png" alt="twitter logo" height={32} width={32} className="rounded-lg" />
         </div>
         <div className="flex items-center justify-between pt-2">
-          <span className="text-sm text-purple-600 font-medium">{numApps} Apps</span>
+          <span className="text-sm text-purple-600 font-medium">{collection.rating.count} Apps</span>
           <button className="flex items-center gap-2 px-2.5 py-1.5 text-xs bg-black text-white rounded-full hover:bg-neutral-700 transition">
             Explore more
             <ArrowRight className="h-3 w-3" />
